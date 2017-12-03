35c183358f1ae5934a6fb7e6c9f1fa649ea2e205
Davidkim1018 <davidkimspam@gmail.com>
2017-12-03 22:05:08 +0900

    update

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
+    '''image model'''
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User,null=True)
+
+class Comment(TimeStampedModel):
+    '''comment model'''
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User,null=True)
+    image = models.ForeignKey(Image,null=True)
+
+class Like(TimeStampedModel):
+    '''like model'''
+    creator = models.ForeignKey(user_models.User,null=True)
+    image = models.ForeignKey(Image, null=True)
